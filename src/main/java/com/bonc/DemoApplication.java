1df1e0832476db7a1a345f93dfceaa8b76dde17d
liuxiaoying <liuxiaoying@bonc.com.cn>
2019-09-24 17:34:52 +0800

    给注解添加注释

--- src/main/java/com/bonc/DemoApplication.java
+++ src/main/java/com/bonc/DemoApplication.java
@@ -3,10 +3,15 @@ package com.bonc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication 注解：是SprnigBoot项目的核心注解，目的是开启自动配置
+ * 等同于@Configuration ，@EnableAutoConfiguration 和 @ComponentScan 三个配置
+ * @ComponentScan：让spring Boot扫描到Configuration类并把它加入到程序上下文
+ * @Configuration ：等同于spring的XML配置文件；使用Java代码可以检查类型安全
+ * @EnableAutoConfiguration ：自动配置
+ */
 @SpringBootApplication
 public class DemoApplication {
-
-
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/bonc/controller/HelloController.java
+++ src/main/java/com/bonc/controller/HelloController.java
@@ -2,6 +2,10 @@ package com.bonc.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * @RestController： 是@ResponseBody和@Controller的组合注解
+ * @RequestMapping： 配置url映射，可以作用在控制器的某个方法上，也可以作用在此控制器类上
+ */
 @RestController
 @RequestMapping("/demo")
 public class HelloController {
